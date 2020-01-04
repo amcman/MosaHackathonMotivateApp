66807232aefdd7087897b1ad6d58c797b3245d9f
rokujohaven <frogsandsweetchickens@gmail.com>
2020-01-04 17:17:04 -0500

    added the variables

--- User.java
+++ User.java
@@ -1,4 +1,16 @@
+/*	-Holds all information pertinent 
+ * to the user (array list of messages, 
+ * goal due date, name, number   
+ * of days until goal completed,
+ *  preference for frequency of messages, 
+ *  and phone number) 
+ */
+
+//make all instance variables private 
+//make all method public 
 
 public class User {
 
+	
+	
 }
