7cd5f06d81f0ba3f52934f4a04b822c560886e8d
amcman <amcman@seas.upenn.edupenn.edu>
2020-01-05 01:46:48 -0500

    Made a bunch of formatting changes to the home screen. Far from done, but made some progress

--- HomeScreen.java
+++ HomeScreen.java
@@ -3,12 +3,15 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.print.PrinterJob;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
@@ -32,6 +35,7 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
@@ -56,7 +60,7 @@ public class HomeScreen {
 
 	
 	//Stage width
-	private int stageX = 800;
+	private int stageX = 700;
 	//Stage height
 	private int stageY = 900;
 	//Instantiate empty User Class
@@ -64,18 +68,101 @@ public class HomeScreen {
 	
 	public Scene start() {
 		
+		//Setting up the main stage and scene
 		Stage mainStage = new Stage();
-		Group homeGroup = new Group();
-		ScrollPane homeScroll = new ScrollPane(homeGroup);
-	    homeScroll.setHmax(0);
-		homeScroll.setStyle("-fx-background: rgb(255,255,255)");
-		Scene homeScene = new Scene(homeScroll, stageX, stageY);
+		StackPane rootNode = new StackPane();
+		ScrollPane scroll = new ScrollPane(rootNode);
+		// make that node fill entire width
+		scroll.setFitToWidth(true);
+		// set background white
+		scroll.setStyle("-fx-background: white");
+		// Create Scene and link it with ScrollPane can have only one node
+		// If you omit the width and height, the scene will be sized automatically based on the size of
+		// the elements contained
+		Scene homeScene = new Scene(scroll, stageX, stageY);
 		mainStage.setScene(homeScene);
 		mainStage.setTitle("Motivate");
+		// Create and format a title
+		Text title = new Text("Motivate");
+		title.setFont(Font.font("Arial Rounded MT", FontWeight.BOLD, FontPosture.ITALIC, 30));
+		// Create an HBox for the title
+		HBox titleBox = new HBox(title);
+		titleBox.setAlignment(Pos.TOP_CENTER);
+		titleBox.setPadding(new Insets(145, 0, 0, 0));
+		
+		// Gender Image
+		Image image = null;
+		try {
+			image = new Image(new FileInputStream("src/logo.png"), 800, 800, true, true);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		ImageView imageView = new ImageView(image);
+		imageView.setFitHeight(125);
+		imageView.setFitWidth(125);
+		imageView.setPreserveRatio(true);
+
+		HBox img = new HBox(imageView);
+		img.setAlignment(Pos.TOP_CENTER);
+		img.setPadding(new Insets(10,10,10,10));
+
+		//Input Rectangle
+		Rectangle inputRect = new Rectangle(520, 500);
+		inputRect.setFill(Color.rgb(212, 255, 215));
+		inputRect.setStroke(Color.rgb(98, 245, 108));
+		inputRect.setStrokeWidth(1);
+		inputRect.setArcHeight(35.0);
+		inputRect.setArcWidth(25.0);
+		//Putting input box within a child vbox
+		VBox inputRectangle = new VBox();
+		inputRectangle.getChildren().add(inputRect);
+		inputRectangle.setViewOrder(5.0);
+		inputRectangle.setAlignment(Pos.BOTTOM_CENTER);
+		inputRectangle.setPadding(new Insets(200,0,50,0));
+		
+		//Name input field
+		TextField nameInput = new TextField();
+		nameInput.setMinWidth(150);
+		HBox nameBox = new HBox(nameInput);
+		nameBox.setViewOrder(0.0);
+		nameBox.setAlignment(Pos.CENTER);
+		nameBox.setPadding(new Insets(0,50,0,0));
+		
+		//Goal input field
+		TextField goalInput = new TextField();
+		goalInput.setMinWidth(150);
+		HBox goalBox = new HBox(goalInput);
+		goalBox.setViewOrder(0.0);
+		goalBox.setAlignment(Pos.CENTER);
+		goalBox.setPadding(new Insets(150,50,0,0));
+		
+		//Frequency Dropdown
+		ComboBox frequencyComboBox = new ComboBox();
+		frequencyComboBox.getItems().add("Twice a day");
+		frequencyComboBox.getItems().add("Once a day");
+		frequencyComboBox.getItems().add("Every other day");
+		frequencyComboBox.getItems().add("Once a week");		
+		HBox comboHBox = new HBox(frequencyComboBox);
+		goalBox.setViewOrder(0.0);
+		goalBox.setAlignment(Pos.CENTER);
+		goalBox.setPadding(new Insets(300,50,0,0));
 		
 
+		//Date picking field for goal end date
+		DatePicker datePicker = new DatePicker();
+	    HBox theDatePicker = new HBox(datePicker);
+	    theDatePicker.setAlignment(Pos.CENTER);
+	    theDatePicker.setPadding(new Insets(450,25,0,0));
+	    
+
+		//Add all children to master VBox
+		rootNode.getChildren().addAll(img,titleBox,inputRectangle,
+				nameBox,goalBox,frequencyComboBox,theDatePicker);
+		//Show the stage
 		mainStage.show();
-		
+
 		return homeScene;
 		
 	}
