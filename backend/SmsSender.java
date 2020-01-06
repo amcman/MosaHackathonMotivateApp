081e3468c45f2623799a0dc82ad79a672c01ff3c
shaerlisa <shaerlisa@xunjings-air.lan>
2020-01-06 16:48:15 -0500

    Created SmsSender.java that sends messages to a user's phone

---
+++ backend/SmsSender.java
@@ -0,0 +1,27 @@
+package backend;
+
+//Install the Java helper library from twilio.com/docs/libraries/java
+import com.twilio.Twilio; 
+import com.twilio.rest.api.v2010.account.Message; 
+import com.twilio.type.PhoneNumber;
+
+public class SmsSender {
+	// Find your Account Sid and Auth Token at twilio.com/console
+    public static final String ACCOUNT_SID =
+            "AC0adc56719ee8405ae27948c292d0456a";
+    public static final String AUTH_TOKEN =
+            "469722a6cbeb5a8c44d5917e4ef87fa4";
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        Message message = Message
+                .creator(new PhoneNumber("+19086721894"), // to
+                        new PhoneNumber("+13605295695" + 
+                        		""), // from
+                        "Welcome to Motivate!!!! We are excited to help you accomplish your goal!")
+                .create();
+
+        System.out.println(message.getSid());
+    }
+}
