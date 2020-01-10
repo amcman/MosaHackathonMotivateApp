9999b80a83592c5d383ce778721aed960caefcb2
shaerlisa <shaerlisa@xunjings-air.lan>
2020-01-09 20:08:06 -0500

    Updated the method to calculate the number of days

--- backend/ValueCalculators.java
+++ backend/ValueCalculators.java
@@ -1,5 +1,34 @@
 package backend;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class ValueCalculators {
 
+	User user; 
+	
+	public ValueCalculators() {
+		this.user = new User(); 
+	}
+	
+	Date startDate = user.getStartDate(); 
+	Date endDate = user.getEndDate();
+	
+	//TO MODIFY
+	String frequency = user.getdailyFreq();
+	
+	// Calculates the number of days the goal will last 
+	public long getNumOfDays() {
+		long durationInMilliSec = endDate.getTime() - startDate.getTime();
+		return TimeUnit.MILLISECONDS.toDays(durationInMilliSec); 
+	}
+	
+	//TO MODIFY
+	// Calculates the number of msgs needed/to be sent
+	public int getNumOfMsgs() {
+		
+		return 0; 
+	}
+	
+	
 }
