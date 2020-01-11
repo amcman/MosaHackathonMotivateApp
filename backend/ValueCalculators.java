87dc392bf538023aa374020d304c757278afeb6a
shaerlisa <shaerlisa@xunjings-air.lan>
2020-01-11 18:25:29 -0500

    In the ValueCalculator, added one method to calc sleep cycle and another one to get random time duration

--- backend/ValueCalculators.java
+++ backend/ValueCalculators.java
@@ -16,7 +16,7 @@ public class ValueCalculators {
 		return (int) TimeUnit.MILLISECONDS.toDays(durationInMilliSec); 
 	}
 	
-	
+	// Helper method to convert frequency from String to int
 	public double getFreqNum() {
 		
 		double freq=0; 
@@ -39,11 +39,24 @@ public class ValueCalculators {
 		return freq; 
 		
 	}
+	
 	// Calculates the number of msgs needed/to be sent
 	public int getNumOfMsgs() {
 			
 		return (int) (getNumOfDays() * getFreqNum()); 
 	}
 	
+	// TO DO for Helen
+		// Calculate the sleep cycle: goal duration / frequency (in milliseconds)
+	public int calcSleepCycle() {
+		return 0; 
+	}
+	
+	// TO DO for Xunjing
+	// Generates a random duration in milliseconds within the desired time window
+	public int getRandomDuration(){
+			
+		return 0; 
+	}
 	
 }
